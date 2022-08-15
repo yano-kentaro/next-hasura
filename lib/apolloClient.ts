feb05fe013b149b9fe3d6dd04455b66b97f27828
ken <ynkn0829@gmail.com>
2022-08-15 15:16:46 +0900

    環境変数の設定

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -14,7 +14,7 @@ import 'cross-fetch/polyfill'
 
 // ----------------------------------
 // export
-export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
+// export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
 
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined
 
@@ -23,7 +23,10 @@ const createApolloClient = () => {
         ssrMode: typeof window === 'undefined',
         cache: new InMemoryCache(),
         link: new HttpLink({
-            uri: 'https://first-hasura-220809.hasura.app/v1/graphql',
+            uri: process.env.NEXT_PUBLIC_HASURA_ENDPOINT,
+            headers: {
+                "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET
+            }
         })
     })
 }
